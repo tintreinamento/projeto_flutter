fcddd93d7fc3467108a45f84a33a839cc9fab008
weversonlimanm <weverson.lima@novomundo.com.br>
2021-06-08 14:49:10 -0300

    Mesclagem: branch componentes

--- lib/views/homepage/homepage.dart
+++ lib/views/homepage/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_flutter/componentes/appbar.dart';
+import 'package:projeto_flutter/componentes/drawer.dart';
 import '../menu/menu.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -29,12 +31,8 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      drawer: MenuPrincipal(),
+      appBar: AppBarComponente(appBar: AppBar()),
+      drawer: DrawerComponente(),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
