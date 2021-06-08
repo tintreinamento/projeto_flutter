340d9c8f84d6f20265adb4c22d1b1b1201465229
weversonlimanm <weverson.lima@novomundo.com.br>
2021-06-08 16:10:32 -0300

    Tarefa: criando tela de pedido venda

--- lib/views/pedido_venda/pedidovenda.dart
+++ lib/views/pedido_venda/pedidovenda.dart
@@ -21,12 +21,21 @@ class _PedidoVendaState extends State<PedidoVenda> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Container(
-                width: 330,
-                height: 120,
-                margin: EdgeInsets.only(left: 20, right: 20),
-                decoration: BoxDecoration(
-                    border: Border.all(
-                        color: Color.fromRGBO(191, 188, 188, 1), width: 1))),
+              width: 330,
+              height: 120,
+              margin: EdgeInsets.only(left: 20, right: 20),
+              decoration: BoxDecoration(
+                  border: Border.all(
+                      color: Color.fromRGBO(191, 188, 188, 1), width: 1)),
+              child: Column(
+                children: [
+                  Row(
+                    children: [Text('CPF/CNPJ'), TextField()],
+                  ),
+                  ElevatedButton(onPressed: () {}, child: Text('Consulta'))
+                ],
+              ),
+            ),
             Text(
               'Pedido venda',
             ),
