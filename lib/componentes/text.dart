9e783472997cad0eaf2996bb76f42c82da05b8f4
weversonlimanm <weverson.lima@novomundo.com.br>
2021-06-09 09:09:10 -0300

    Tarefa: criando componente de textformfield

--- lib/componentes/text.dart
+++ lib/componentes/text.dart
@@ -3,5 +3,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 Widget textComponente(String label) {
-  return Text(label);
+  return Text(
+    label,
+    style: TextStyle(
+      fontFamily: 'Roboto',
+      fontWeight: FontWeight.w400,
+      fontSize: 24,
+      color: Color.fromRGBO(0, 0, 0, 1),
+    ),
+  );
 }
