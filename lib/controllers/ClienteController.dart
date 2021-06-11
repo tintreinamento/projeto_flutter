3832d20f2f15db658570b4e6a1aff9b44556b5b0
Nathan <nat.hando@hotmail.com>
2021-06-11 17:44:09 -0300

    Adiciona criação de cliente

--- lib/controllers/ClienteController.dart
+++ lib/controllers/ClienteController.dart
@@ -1,29 +1,28 @@
-import 'dart:collection';
 import 'dart:convert';
-import 'package:projeto_flutter/models/Cliente.dart';
+import 'package:projeto_flutter/models/ClienteModel.dart';
 import 'package:projeto_flutter/services/Api.dart';
 
 class ClienteController {
-  Future<List<Cliente>> obtenhaClientes() async {
+  Future<List<ClienteModel>> obtenhaClientes() async {
     final resposta = await new Api().obtenha('clientes');
 
     if (resposta.statusCode != 200) {
       throw new Exception("Falha ao obter os dados na api");
     }
 
-    List<Cliente> colecaoDeClientes = new List.empty(growable: true);
+    List<ClienteModel> colecaoDeClientes = new List.empty(growable: true);
 
     List<dynamic> stringJson = json.decode(resposta.body);
 
     stringJson.forEach((element) {
-      var cliente = new Cliente.fromJson(element);
+      var cliente = new ClienteModel.fromJson(element);
       colecaoDeClientes.add(cliente);
     });
 
     return colecaoDeClientes;
   }
 
-  Future<Cliente> obtenhaClienteCpf(String cpf) async {
+  Future<ClienteModel> obtenhaClientePorCpf(String cpf) async {
     final resposta = await new Api().obtenha('clientes?cpf=' + cpf);
 
     if (resposta.statusCode != 200) {
@@ -32,6 +31,19 @@ class ClienteController {
 
     var stringJson = json.decode(resposta.body);
 
-    return new Cliente.fromJson(stringJson.single);
+    return new ClienteModel.fromJson(stringJson.single);
+  }
+
+  Future<ClienteModel> crieCliente(ClienteModel cliente) async {
+    print(json.encode(cliente));
+    final resposta = await new Api().crie('clientes', json.encode(cliente));
+
+    if (resposta.statusCode != 200) {
+      throw new Exception("Falha ao comunicar com a api");
+    }
+
+    var stringJson = json.decode(resposta.body);
+
+    return new ClienteModel.fromJson(stringJson.single);
   }
 }
