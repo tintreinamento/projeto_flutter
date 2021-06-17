39bede03f8d383bf18ca16c400aa858f9d9e7530
Nardelli17 <vitornardelli17@gmail.com>
2021-06-17 20:14:24 -0300

    Layout ajustado com Expand

--- lib/views/splash/splash_screen.dart
+++ lib/views/splash/splash_screen.dart
@@ -24,13 +24,13 @@ class _MySplashPageState extends State<MySplashPage> {
         alignment: Alignment.center,
         child: Stack(
           children: <Widget>[
-            Container(
-              width: 367,
-              height: 792,
-              margin: EdgeInsets.only(bottom: 109),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                color: Color(0xffc4c4c4),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.only(bottom: 109),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Color(0xffc4c4c4),
+                ),
               ),
             ),
             Positioned.fill(
@@ -41,32 +41,36 @@ class _MySplashPageState extends State<MySplashPage> {
                   height: 1485,
                   child: Stack(
                     children: <Widget>[
-                      Positioned(
-                        left: 0,
-                        top: 300,
-                        child: Transform.scale(
-                          scale: 1.8,
-                          child: Transform.rotate(
-                            angle: -0.86,
-                            child: Container(
-                              width: 489.97,
-                              height: 958.37,
-                              color: Color(0xffce0505),
+                      Expanded(
+                        child: Positioned(
+                          left: 0,
+                          top: 300,
+                          child: Transform.scale(
+                            scale: 1.8,
+                            child: Transform.rotate(
+                              angle: -0.86,
+                              child: Container(
+                                width: 489.97,
+                                height: 958.37,
+                                color: Color(0xffce0505),
+                              ),
                             ),
                           ),
                         ),
                       ),
-                      Positioned(
-                        left: 355.84,
-                        top: 0,
-                        child: Transform.scale(
-                          scale: 1.85,
-                          child: Transform.rotate(
-                            angle: -0.66,
-                            child: Container(
-                              width: 369.97,
-                              height: 650,
-                              color: Color(0xff005db4),
+                      Expanded(
+                        child: Positioned(
+                          left: 355.84,
+                          top: 0,
+                          child: Transform.scale(
+                            scale: 1.85,
+                            child: Transform.rotate(
+                              angle: -0.66,
+                              child: Container(
+                                width: 369.97,
+                                height: 650,
+                                color: Color(0xff005db4),
+                              ),
                             ),
                           ),
                         ),
