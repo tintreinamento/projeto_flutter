11472f67a16d26341190eeaea564cefa69326731
Nathan <nat.hando@hotmail.com>
2021-06-18 17:31:52 -0300

    Insere obtenhaTodos e corrige obtenção por id

--- lib/controllers/EstadoController.dart
+++ lib/controllers/EstadoController.dart
@@ -3,10 +3,26 @@ import 'package:projeto_flutter/models/EstadoModel.dart';
 import 'package:projeto_flutter/services/Api.dart';
 
 class EstadoController {
-  Future<EstadoModel> obtenhaPorId(String id) async {
-    final resposta = await new Api().obtenha('estados?id=' + id);
+  Future<List<EstadoModel>> obtenhaTodos() async {
+    final resposta = await new Api().obtenha('estados');
+
+    List<EstadoModel> colecaoDeItemPedido = new List.empty(growable: true);
+
+    List<dynamic> stringJson = json.decode(resposta.body);
+
+    stringJson.forEach((element) {
+      var pedido = new EstadoModel.fromJson(element);
+      colecaoDeItemPedido.add(pedido);
+    });
+
+    return colecaoDeItemPedido;
+  }
+
+  Future<EstadoModel> obtenhaPorId(int id) async {
+    final resposta = await new Api().obtenha('estados/' + id.toString());
 
     var stringJson = json.decode(resposta.body);
+
     return new EstadoModel.fromJson(stringJson.single);
   }
 
