04782056881c9dfe2278d432511c288fdd6d7f48
Nathan <nat.hando@hotmail.com>
2021-06-18 19:34:02 -0300

    Insere método obtenhaTodos

--- lib/controllers/CidadeController.dart
+++ lib/controllers/CidadeController.dart
@@ -3,6 +3,21 @@ import 'package:projeto_flutter/models/CidadeModel.dart';
 import 'package:projeto_flutter/services/Api.dart';
 
 class CidadeController {
+  Future<List<CidadeModel>> obtenhaTodos() async {
+    final resposta = await new Api().obtenha('cidades');
+
+    List<CidadeModel> colecaoDeClientes = new List.empty(growable: true);
+
+    List<dynamic> stringJson = json.decode(resposta.body);
+
+    stringJson.forEach((element) {
+      var cliente = new CidadeModel.fromJson(element);
+      colecaoDeClientes.add(cliente);
+    });
+
+    return colecaoDeClientes;
+  }
+
   Future<CidadeModel> obtenhaPorId(int id) async {
     final resposta = await new Api().obtenha('cidades/' + id.toString());
 
