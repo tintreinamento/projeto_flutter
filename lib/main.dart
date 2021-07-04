f59aaadf5b390b683a06d13bf010fd7ee5987bce
Yuri Cassimiro <y.cassimiro@gmail.com>
2021-07-04 19:05:25 -0300

    Atualiza rotas e remove comentários desnecessários

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,11 @@ import 'package:projeto_flutter/views/cliente/consultar/ClienteConsultarView.dar
 import 'package:projeto_flutter/views/estoque/EstoqueView.dart';
 import 'package:projeto_flutter/views/fornecedor/FornecedorCadastrarView.dart';
 import 'package:projeto_flutter/views/fornecedor/FornecedorConsultarView.dart';
+import 'package:projeto_flutter/views/pedido_Compra/consulta/PedidoCompraConsultaView.dart';
 import 'package:projeto_flutter/views/pedido_venda/cadastrar/PedidoVendaCadastrarView.dart';
 import 'package:projeto_flutter/views/pedido_venda/consulta/PedidoVendaConsultaView.dart';
-import 'package:projeto_flutter/views/pedido_Compra/PedidoCompraCadastroView.dart';
 import 'package:projeto_flutter/views/produto/consulta_produto.dart';
 import 'package:projeto_flutter/views/login/LoginView.dart';
-import 'package:projeto_flutter/views/produto/ProdutoCadastrarView.dart';
 import 'package:projeto_flutter/views/splash/splash_screen.dart';
 // import 'package:projeto_flutter/views/pedido/pedido.dart';
 // import './views/pedido/pedido.dart';
@@ -24,38 +23,21 @@ void main() {
     providers: [ChangeNotifierProvider(create: (context) => CarrinhoModel())],
     child: MyApp(),
   ));
-  //runApp(MyApp());
 }
 
-// void main() {
-//   runApp(MyApp());
-//   runApp(MyApp());
-// }
-
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       initialRoute: '/pedido_venda_cadastrar',
       routes: {
-        '/': (context) => PedidoVendaConsultaView(),
-        //'/': (context) => SplashPage(), //ok
-        //'/login': (context) => LoginView(), // ok
+        '/': (context) => SplashPage(), //ok
+        '/login': (context) => LoginView(), // ok
         '/pedido_venda_cadastrar': (context) => PedidoVendaCadastraView(),
         '/pedido_venda_consultar': (context) => PedidoVendaConsultaView(),
         '/cadastrar_cliente': (contexto) => ClienteCadastroView(),
@@ -63,7 +45,9 @@ class MyApp extends StatelessWidget {
         '/cadastrar_fornecedor': (context) => FornecedorCadastrarView(),
         '/consultar_fornecedor': (context) => FornecedorConsultarView(),
         '/consultar_produto': (context) => ProdutoConsultarView(),
-        // '/pedido_compra': (contexto) => PedidoCompraCadastroView()
+        //'/pedido_compra_cadastrar': (contexto) => PedidoCompraCadastroView(),
+        '/pedido_compra_consultar': (contexto) => PedidoCompraConsultaView(),
+        //'/estoque': (contexto) => EstoqueView(),
         '/cadastrar_precificacao': (context) => PrecificacaoView(),
       },
     );
