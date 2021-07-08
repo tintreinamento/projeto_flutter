0fe9c4cc219b2366002047dba1fdc61b75b360c3
Nathan <nat.hando@hotmail.com>
2021-07-08 16:05:04 -0300

    Altera EstoqueModel

--- lib/models/EstoqueModel.dart
+++ lib/models/EstoqueModel.dart
@@ -1,27 +1,17 @@
 class EstoqueModel {
   var id;
-  var idProduto;
-  var quantidade;
   var nome;
 
   EstoqueModel({
     this.id,
-    this.idProduto,
-    this.quantidade,
     this.nome,
   });
 
   factory EstoqueModel.fromJson(Map<String, dynamic> parsedJson) {
-    return EstoqueModel(
-        id: parsedJson['ID_ESTOQUE'],
-        idProduto: parsedJson['ID_PRODUTO'],
-        quantidade: parsedJson['QUANTIDADE'],
-        nome: parsedJson['NOME']);
+    return EstoqueModel(id: parsedJson['ID_ESTOQUE'], nome: parsedJson['NOME']);
   }
 
   Map<String, dynamic> toJson() => {
-        'ID_PRODUTO': idProduto,
-        'QUANTIDADE': quantidade,
         'NOME': nome,
       };
 }
