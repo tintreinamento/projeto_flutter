cc228aa6ea83ffe26d6a1ab716da1950b70d7b65
Nardelli17 <vitornardelli17@gmail.com>
2021-07-08 09:14:07 -0300

    Tarefa: main correta

--- lib/main.dart
+++ lib/main.dart
@@ -38,8 +38,7 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        //'/': (context) => SplashPage(), //ok
-        '/': (context) => FornecedorCadastrarView(), //ok
+        '/': (context) => SplashPage(), //ok
         '/login': (context) => LoginView(), // ok
         '/pedido_venda_cadastrar': (context) => PedidoVendaCadastraView(),
         '/pedido_venda_consultar': (context) => PedidoVendaConsultaView(),
